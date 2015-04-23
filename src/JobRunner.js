94021da8323d706da44e0e2dfb861667abf04ac1
Johannes Tegnér <jitedev@gmail.com>
2015-04-23 20:16:21 +0200

    Rewrote script in a more object oriented approach. The jobs have been moved to their own file 'JobRunner' and all the 'process.kill()' calls have been removed.

---
+++ src/JobRunner.js
@@ -0,0 +1,84 @@
+﻿var util = require('util');
+
+/**
+ * Wrapper for jobs.
+ */
+function JobRunner(data, baseDir) {
+    
+    var _self = this;
+    var _jobPath = util.format('%s/%s/%s', process.cwd(), baseDir, data.path);
+    var _job = require(_jobPath);
+    var _interval = data.interval;
+    var _hInt = null;
+    var _stopped = false;
+    
+    /**
+     * Get name of the current job.
+     * @param {string} Name.
+     */
+    this.getName = function getName() {
+        return _job.getName();
+    }
+    
+    /**
+     * Get current job version.
+     * @param {string} Version. 
+     */
+    this.getVersion = function getVersion() {
+        return _job.getVersion();
+    }
+    
+    /**
+     * Get job interval.
+     * @returns {int} Interval in minutes.
+     */
+    this.getInterval = function getInterval() {
+        return _interval;
+    };
+    
+    /**
+     * Internal job loop.
+     */
+    var _run = function _run() {
+        if (_stopped) {
+            return;
+        }
+        _hInt = setInterval(function () {
+            // Tick!  Clear the interval, cause we don't want to start the job-timer till after its done.
+            clearInterval(_hInt);
+            // Run the actual job...
+            _job.runJob(function (error, message, time) {
+                // Job is done, alert notifiers.
+                notifiers.asyncForEach(function (notifier, cb) {
+                    if (error) {
+                        notifier.sendError(error, cb);
+                    } else {
+                        notifier.sendSuccess(message, time, cb);
+                    }
+                }, function () {
+                    // All notifiers alerted, re-start the job!
+                    _run();
+                });
+            });
+        }, _interval * 1000 * 60);
+    };
+    
+    /**
+     * Run a job loop.
+     */
+    this.run = function run() {
+        log.info('Starting the job %s.', _self.getName());
+        _run();
+    };
+    
+    /**
+     * Stop the job.
+     */
+    this.stop = function stop() {
+        log.info('The job "%s" has been stopped. If its currently in the midle of a job, this will finish first.', _self.getName());
+        _stopped = true;
+        clearInterval(_hInt);
+    };
+}
+
+module.exports = JobRunner;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,154 @@
 #! /usr/bin/env node
 process.title = 'Continual';
 require('./array-functions.js');
-var log     = require('node-yolog');
-var fs      = require('fs');
-var util    = require('util');
 
-var eol = process.platform === 'win32' ? '\r\n' : '\n';
-var continualDir = '.continual';
-var jobs = [];
-var notifiers = [];
+global.log = require('node-yolog');
+
+var util      = require('util');
+var JobRunner = require('./JobRunner.js'); 
+
+/**
+ * Continual main constructor.
+ */
+function Continual() {
+
+    var _self           = this;
+    var _fs             = require('fs');
+    var _eol            = process.platform === 'win32' ? '\r\n' : '\n';
+    var _dir            = '.continual';
+    var _configFile     = _dir + '/config.json';
+    var _jobsFolder     = _dir + '/jobs';
+    var _notifierFolder = _dir + '/notifiers';
+    
+
+    this.jobs      = [];
+    this.notifiers = [];
+    this.config    = {};
+    
+    /** 
+     * Available commands.
+     */
+    var _commands = [
+        {
+            'command': '-commands'
+            , 'action': 'Show this command list.'
+        },
+        {
+            'command': '-init    ' // Spaces to format output neater.
+            , 'action': 'Initialize continual in current directory.'
+        }
+    ];
+    
+    /**
+     * Initialize Continual.
+     * This will, if not successfull, create all continual folders required for
+     * continual to run, in the current directory.
+     */
+    this.init = function init() {
+        log.info('Initializing .continual in current directory.');
+        
+        if (_fs.existsSync(_dir)) {
+            log.error('There is already a %s directory in this directory. Please use that one or remove it and run init command again.', _dir);
+        } else {
+            _fs.mkdirSync(_dir);
+            if (!_fs.existsSync(_dir)) {
+                log.error('Failed to create continual directory.');
+            } else {
+                var fileData = util.format('{%s\t"notifiers": [],%s\t"jobs": []%s}', _eol, _eol, _eol);
+                _fs.writeFileSync(_configFile, fileData, 'UTF8');
+                if (!_fs.existsSync(_configFile)) {
+                    log.error('Failed to create config file.');
+                }
+                _fs.mkdir(_jobsFolder);
+                _fs.mkdir(_notifierFolder);
+            }
+        }
+    };
+    
+    /**
+     * Print help command.
+     */
+    this.help = function help() {
+        // Show the commands using standard output, not the yolog instance.
+        console.log('Available continual commands:');
+        for(command in _commands) {
+            console.log('\t' + command.command + '\t' + command.action);
+        }
+    };
+    
+    /**
+     * Load the config data file.
+     * @returns {boolean|object} Config object on success else false.
+     */
+    var loadConfig = function loadConfig() {
+        // Load config file.
+        var config = require(process.cwd() + '/' + _configFile, 'UTF8');
+        if (config.jobs.length === 0 || config.notifiers.length === 0) {
+            log.error('Jobs and Notifiers can not be empty.');
+            return false;
+        }
+        return config;
+    };
+    
+    /**
+     * Load jobs from the config file.
+     * @returns {boolean} True on success else false.
+     */
+    var loadJobs = function loadJobs(config) {
+        // Fetch the jobs to use.
+        config.jobs.asyncMap(function (jobData, next) {
+            var runner = new JobRunner(jobData, _dir);
+            log.info('Loaded job: %s (%s) - Interval: %d', runner.getName(), runner.getVersion(), runner.getInterval());
+            next(runner);
+        }, function (result) {
+            _self.jobs = result;
+            log.debug('All (%d) jobs loaded.', _self.jobs.length);
+        });
+        return true;
+    }
+    
+    /**
+     * Load notifiers from the config file.
+     * @returns {boolean} True on success else false.
+     */
+    var loadNotifiers = function loadNotifiers(config) {
+        // Fetch the notifier to use.
+        config.notifiers.asyncMap(function (notifierData, next) {
+            var notifier = require(process.cwd() + '/' + _dir + '/' + notifierData);
+            log.info("Loaded notifier: %s (%s)", notifier.getName(), notifier.getVersion());
+            next(notifier);
+        }, function (result) {
+            _self.notifiers = result;
+            log.debug('All (%d) notifiers loaded.', _self.notifiers.length);
+        });
+        return true;
+    }
+
+    /**
+     * Load continual in current directory.
+     * @returns {boolean} True on success else false.
+     */ 
+    this.load = function load() {
+        if (!_fs.existsSync(_dir) || !_fs.existsSync(_configFile)) {
+            log.error('Failed to locate continual files in directory. Have you initialized continual?');
+            log.info('Type `continual -commands` for commands.');
+            return false;
+        }
+        var config = loadConfig();    
+        if (!config) {
+            return false;
+        }
+        if (!loadNotifiers(config)) {
+            return false;
+        }
+        if (!loadJobs(config)) {
+            return false;
+        }
+        return true;
+    };
+}
+
+var continual = new Continual();
 
 var init = process.argv.find(function(element, index, array) {
     return element.indexOf('-init') !== -1;
@@ -17,109 +157,26 @@ var help = process.argv.find(function(element, index, array) {
    return element.indexOf('-commands') !== -1;
 });
 
-if(help) {
+var debug = process.argv.find(function (element, index, array) {
+    return element.indexOf('-debug') !== -1;
+});
 
-    console.log('Commands:');
-    console.log('\t-commands\tShows this command list.');
-    console.log('\t-init    \tInitializes continual in current directory.');
-
-    process.kill();
+if (!debug) {
+    log.set(false, 'debug', 'trace', 'todo');
 }
 
-if(init) {
-    log.info('Initializing .continual in current directory.');
-
-    if(fs.existsSync(continualDir)) {
-        log.error('There is already a %s directory in this directory. Please use that one or remove it and run init command again.', continualDir);
-    } else {
-        fs.mkdirSync(continualDir);
-        if(!fs.existsSync(continualDir)) {
-            log.error('Failed to create continual directory.');
-        } else {
-            var file = util.format('{%s\t"notifiers": [],%s\t"jobs": []%s}', eol, eol, eol);
-            fs.writeFileSync(continualDir + '/config.json', file, 'UTF8');
-            if(!fs.existsSync(continualDir + '/config.json')) {
-                log.error('Failed to create config file.');
-            }
-            fs.mkdir('jobs');
-            fs.mkdir('notifiers');
-        }
-    }
-    process.kill();
+if (help) {
+    continual.help();
+} else if (init) {
+    continual.init();
 } else {
-    if(!fs.existsSync(continualDir) || !fs.existsSync(continualDir + '/config.json')) {
-        log.error('Failed to locate continual files in directory. Have you initialized continual?');
-        log.info('Type `continual -commands` for commands.');
-        process.kill();
+    if (continual.load()) {
+        log.info("Continual loaded and ready. Starting jobs.");
+        continual.jobs.asyncForEach(function (job, d) {
+            job.run();
+            d();
+        }, function () {
+            log.info('All jobs started.');
+        });
     }
 }
-
-// Read config file.
-var config = require(process.cwd() + '/' + continualDir + '/config.json', 'UTF8');
-
-if(config.jobs.length === 0 || config.notifiers.length === 0) {
-    log.error('Jobs and Notifiers can not be empty.');
-    process.kill();
-}
-
-// Fetch the notifier to use.
-config.notifiers.asyncMap(function(notifierData, next) {
-    var notifier = require(process.cwd() + '/' + continualDir + '/' + notifierData);
-    log.info("Loaded notifier: %s (%s)", notifier.getName(), notifier.getVersion());
-    next(notifier);
-}, function(result) {
-    notifiers = result;
-    log.debug('All (%d) notifiers loaded.', notifiers.length);
-});
-
-// Fetch the jobs to use.
-config.jobs.asyncMap(function(jobData, next) {
-    var job = {
-        'job': require(process.cwd() + '/' + continualDir + '/' + jobData.path)
-        , 'interval': jobData.interval
-    };
-    log.info('Loaded job: %s (%s) - Interval: %d', job.job.getName(), job.job.getVersion(), job.interval);
-    next(job);
-}, function(result) {
-    jobs = result;
-    log.debug('All (%d) jobs loaded.', jobs.length);
-});
-
-/**
- * Run a job loop.
- * @param {object} job job to run (need to implement the job-interface).
- * @param {int} interval Interval to run the job at (in minutes).
- */
-var run = function run(job, interval) {
-    var handle = setInterval(function() {
-        log.info('Starting %s...', job.getName());
-        clearInterval(handle);
-
-        job.runJob(function(error, message, time) {
-            notifiers.asyncForEach(function(notifier, cb) {
-                if(error) {
-                    notifier.sendError(error, function() {
-                        cb();
-                    });
-                } else {
-                    notifier.sendSuccess(message, time, function() {
-                        cb();
-                    });
-                }
-            }, function() {
-                log.info('%s Completed in %d s.', job.getName(), (time / 1000));
-                log.info('Waiting for %s minutes, then running job again.', interval);
-                run(job, interval);
-            });
-        });
-    },  interval * 60 * 1000);
-};
-
-log.info("Continual loaded and ready. Starting jobs.");
-jobs.asyncForEach(function(job, d) {
-    run(job.job, job.interval);
-    d();
-}, function() {
-    log.info('All jobs started.');
-});
-
